87aa2ff67239cc35932919012d163f92d193f74e
awanthairushan <2019is067@stu.ucsc.cmb.ac.lk>
2022-08-23 23:23:20 +0530

    created login backend

--- routes/Occupants.js
+++ routes/Occupants.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const {Occupants, Users} = require("../models");
 const bcrypt = require("bcrypt");
+const jsonwebtoken = require("jsonwebtoken");
 
 router.post("/signup", async (req, res) => {
     const {email, name, contact, location, password} = req.body;
@@ -12,6 +13,7 @@ router.post("/signup", async (req, res) => {
             contact: contact,
             location: location,
             password: hash,
+            role: 2,
         });
         res.json("SUCCESS");
     });
@@ -21,15 +23,21 @@ router.post("/login", async (req, res) => {
     const {email, password} = req.body;
 
     const occupant = await Occupants.findOne({where: {email: email}});
-
+    console.log(occupant)
     if (!occupant) {
-        res.json({error: "User doesn't exist"});
+        res.json({state: 0, error: "User doesn't exist"});
     } else {
         bcrypt.compare(password, occupant.password).then((match) => {
-            if (!match) {
+           if (!match) {
                 res.json({error: "Invalid username or password"});
             } else {
-                res.json("Logged in");
+                const accessToken = jsonwebtoken.sign(
+                    {role: occupant.role, id:occupant.id},
+                    "secretaryship",
+                    {expiresIn: '1h'}
+                );
+                //console.log(occupant);
+                res.json({state: 1, data: { role: occupant.role, token: accessToken}});
             }
         });
     }
