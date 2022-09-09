fed68eba9c77305ad01531a25a86a19a9836e3ee
Tharaki-99 <dilshanitharaki@gmail.com>
2022-09-09 09:49:37 +0530

    partially done

---
+++ models/Payments.js
@@ -0,0 +1,33 @@
+module.exports = (sequelize, DataTypes) => {
+    const Payments = sequelize.define("Payments", {
+        rent_date: {
+            type: DataTypes.DATE,
+            allowNull: false,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        renter_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        renter_email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        amount: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        status: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    });
+    return Payments;
+};
