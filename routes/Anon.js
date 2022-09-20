3d57127734b6db0f91782480c71af0d572192bd6
Thushan Dananjana <62166556+Thushan98@users.noreply.github.com>
2022-09-20 12:01:48 +0530

    update query-1

--- routes/Anon.js
+++ routes/Anon.js
@@ -22,9 +22,13 @@ router.get("/bestplaces", async (req, res) => {
 //latest place
 
 router.get("/latestplaces", async (req, res) => {
-    
-    // const {email, password} = req.body;  
-    const places = await Places.findAll();
+    const endDate = Date.now();
+    const startDate= endDate -7;
+    //const sevenDaysAgo = new Date(new Date().setDate(new Date().getDate() - 30));
+    const places = await Places.findAll({
+        where: {between : [endDate, startDate]}
+     } 
+    );
     console.log(places)
     if (!places) {
         res.json({ state: 0, error: "User doesn't exist" });
