8b13f3d576e6f0139a2b91cdd66c78929512ddb3
Semini48 <84171007+Semini48@users.noreply.github.com>
2022-10-25 00:13:37 +0530

    done

--- routes/Renter.js
+++ routes/Renter.js
@@ -3,37 +3,44 @@ const router = express.Router();
 const { Transactions } = require("../models");
 const bcrypt = require("bcrypt");
 const jsonwebtoken = require("jsonwebtoken");
-const sequelize = require("sequelize")
+const sequelize = require("sequelize");
 
 //select occupants
-router.get("/transactions", async (req, res) => {
-  // try {
-  //   const transaction = await db.Transactions.findOne({
-  //     attributes:['renter_id', [sequelize.fn("SUM", sequelize.col("amount"))]],
-  //     group: ['renter_id'],
-  //     raw: true
-  //   });
-  //   res.send(transaction);
-  // } catch (err) {
-  //   res.send(err);
-  // }
+router.get("/transactions/:id", async (req, res) => {
+  const renterId = req.params.id;
   const transaction = await Transactions.findAll({
-      attributes: [
-        'renter_id',
-        [sequelize.fn("SUM", sequelize.col('amount')), 'total_amount'],
-      ],
-      group: ['renter_id'],
-    });
+    attributes: [
+      "renter_id",
+      [sequelize.fn("SUM", sequelize.col("amount")), "total_amount"],
+    ],
+    group: ["renter_id"],
+    where: {
+      renter_id: renterId,
+    },
+  });
 
-  // const transaction = await Transactions.findAll({
-  //   attributes: ["renter_id", "amount"],
-  //   group: ["renter_id"],
-  // });
   console.log(transaction);
   if (!transaction) {
     res.json({ state: 0, error: "User doesn't exist" });
   } else {
     res.send(transaction);
+  }
+});
+
+router.get("/paymentofplaces/:id", async (req, res) => {
+  const renterId = req.params.id;
+  const paymentofplaces = await Transactions.findAll({
+    attributes: ["renter_id", "amount", "place_id", "rent_date"],
+    where: {
+      renter_id: renterId,
+    },
+  });
+
+  console.log(paymentofplaces);
+  if (!paymentofplaces) {
+    res.json({ state: 0, error: "User doesn't exist" });
+  } else {
+    res.send(paymentofplaces);
   }
 });
 
