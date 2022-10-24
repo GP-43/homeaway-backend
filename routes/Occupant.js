c6a3ee95007d34f8edadd6ff0f8a9e2311563e51
Tharindu69 <2019cs169@stu.ucsc.cmb.ac.lk>
2022-10-24 19:53:26 +0530

    error

--- routes/Occupant.js
+++ routes/Occupant.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {Places} = require("../models");
+const {Places, Occupants, Complaint} = require("../models");
 
 router.get("/bookings", async (req, res) => {
     const bookings = await Places.findAll();
@@ -10,5 +10,33 @@ router.get("/bookings", async (req, res) => {
         res.json(bookings);
     }
 });
+
+// //get details to profile
+// router.get("/select/profile/:id", async (req, res) => {
+//     const userId = req.params.id;
+//     const profileDetails = await Occupants.findAll({ where: { id: userId } });
+//     console.log(profileDetails)
+//     if (!profileDetails) {
+//         res.json({ state: 0, error: "User doesn't exist" });
+//     } else {
+//         res.send(profileDetails)
+//     }
+// }); 
+
+
+// router.get("/select/rejectedComplaints", async (req, res) => {
+
+//     const profileDetails = await Occupants.findAll(
+//         {
+//             where: { role: 0 } 
+//         }
+//     );
+//     console.log(profileDetails)
+//     if (!profileDetails) {
+//         res.json({ state: 0, error: "User doesn't exist" });
+//     } else {
+//         res.send(profileDetails)
+//     }
+// });
 
 module.exports = router;
