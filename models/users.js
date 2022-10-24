1a5b7cfda1b7be867a8644e69e1c7425ac05df90
awanthairushan <2019is067@stu.ucsc.cmb.ac.lk>
2022-10-24 18:02:08 +0530

    fixed bug in database

---
+++ models/users.js
@@ -0,0 +1,21 @@
+module.exports = (sequelize, DataTypes) => {
+    const Users = sequelize.define("Users", {
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        role: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        }
+    });
+
+    Users.associate = (models) => {
+        Users.hasOne(models.Occupants)
+    };
+    return Users;
+};
--- routes/Authentication.js
+++ routes/Authentication.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {Occupants, Places} = require("../models");
+const {Occupants, Places, Users} = require("../models");
 const bcrypt = require("bcrypt");
 const jsonwebtoken = require("jsonwebtoken");
 
@@ -9,14 +9,23 @@ router.post("/signup", async (req, res) => {
     const existEmail = await Occupants.findOne({where: {email: email}});
     if (existEmail === null) {
         bcrypt.hash(password, 10).then((hash) => {
-            Occupants.create({
-                email: email,
-                name: name,
-                contact: contact,
-                location: location,
+            Users.create({
+                email:email,
                 password: hash,
-                role: 2,
+                role:2,
+            }).then(result =>{
+                Occupants.create({
+                    email: email,
+                    name: name,
+                    contact: contact,
+                    location: location,
+                    password: hash,
+                    role: 2,
+                    UserId: result.id,
+                });
             });
+            debugger
+
             res.json("SUCCESS");
         });
     } else {
@@ -26,47 +35,27 @@ router.post("/signup", async (req, res) => {
 
 router.post("/login", async (req, res) => {
 
+    // admin role -> 1, occupant role -> 2, renter role -> 3
+
     const {email, password} = req.body;
-    const occupant = await Occupants.findOne({where: {email: email}});
-    if (!occupant) {
+    const user = await Occupants.findOne({where: {email: email}});
+    // const occupant = await Occupants.findOne({where: {email: email}});
+    if (!user) {
         res.json({state: 0, error: "User doesn't exist!"});
     } else {
-        bcrypt.compare(password, occupant.password).then((match) => {
+        bcrypt.compare(password, user.password).then((match) => {
             if (!match) {
                 res.json({error: "Invalid username or password"});
             } else {
                 const accessToken = jsonwebtoken.sign(
-                    {role: occupant.role, id: occupant.id},
+                    {role: user.role, id: user.id},
                     "secretaryship",
                     {expiresIn: '1h'}
                 );
-                res.json({state: 1, data: {role: occupant.role, userId: occupant.id, token: accessToken}});
+                res.json({state: 1, data: {role: user.role, userId: user.id, token: accessToken}});
             }
         });
     }
 });
-
-router.get("/bookings", async (req, res) => {
-    const bookings = await Places.findAll();
-    if (!bookings) {
-        res.json({state: 0, error: "User doesn't exist"});
-    } else {
-        res.json(bookings);
-    }
-});
-
-// router.get("/users", async (req, res) => {
-
-//     // const {email, password} = req.body;
-
-//     const occupant = await Occupants.findAll({where: {role: 2}});
-//     console.log(occupant)
-//     if (!occupant) {
-//         res.json({state: 0, error: "User doesn't exist"});
-//     } else {
-//         console.log(occupant)
-//     }
-// });
-
 
 module.exports = router;
