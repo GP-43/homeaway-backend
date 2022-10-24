862b272931f80bd428dedaea8650ce96d7feaab2
Semini48 <84171007+Semini48@users.noreply.github.com>
2022-10-24 20:57:46 +0530

    partially done

--- routes/Renter.js
+++ routes/Renter.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const { Transactions } = require("../models");
 const bcrypt = require("bcrypt");
 const jsonwebtoken = require("jsonwebtoken");
+const sequelize = require("sequelize")
 
 //select occupants
 router.get("/transactions", async (req, res) => {
@@ -16,18 +17,18 @@ router.get("/transactions", async (req, res) => {
   // } catch (err) {
   //   res.send(err);
   // }
-  // const transaction = await Transactions.findAll({
-  //     attributes: [
-  //       'renter_id',
-  //       [sequelize.fn("SUM", sequelize.col('amount')), 'total_amount'],
-  //     ],
-  //     group: ['renter_id'],
-  //   });
-
   const transaction = await Transactions.findAll({
-    attributes: ["renter_id", "amount"],
-    group: ["renter_id"],
-  });
+      attributes: [
+        'renter_id',
+        [sequelize.fn("SUM", sequelize.col('amount')), 'total_amount'],
+      ],
+      group: ['renter_id'],
+    });
+
+  // const transaction = await Transactions.findAll({
+  //   attributes: ["renter_id", "amount"],
+  //   group: ["renter_id"],
+  // });
   console.log(transaction);
   if (!transaction) {
     res.json({ state: 0, error: "User doesn't exist" });
