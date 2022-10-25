b1c0472213fd0427bc86590f450c477f37f4c9ed
Tharaki-99 <dilshanitharaki@gmail.com>
2022-10-25 10:01:27 +0530

    partially done

--- routes/Occupant.js
+++ routes/Occupant.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const {Places} = require("../models");
+const { Places } = require("../models");
 
 router.get("/bookings", async (req, res) => {
     const bookings = await Places.findAll();
     if (!bookings) {
-        res.json({state: 0, error: "User doesn't exist"});
+        res.json({ state: 0, error: "User doesn't exist" });
     } else {
         res.json(bookings);
     }
 });
+
+router.post("/makebooking", async (req, res) => {
+    const {
+
+    } = JSON.parse(req.body.addNewFormData);
+})
 
 module.exports = router;
