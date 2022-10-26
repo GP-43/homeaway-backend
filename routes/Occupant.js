440ec7f07375ecdca86e206137298778dd03cefc
Tharindu69 <2019cs169@stu.ucsc.cmb.ac.lk>
2022-10-26 10:10:57 +0530

    not completed

--- routes/Occupant.js
+++ routes/Occupant.js
@@ -1,6 +1,27 @@
 const express = require('express');
 const router = express.Router();
 const {Places, Occupants, Complaint} = require("../models");
+// const multer = require("multer");
+// const { Places } = require("../models");
+
+// var storage = multer.diskStorage({
+//     destination: function (req, file, callback) {
+//       callback(null, "./public/profileImages/");
+//     },
+//     filename: function (req, file, callback) {
+//       const fileNameSplit = file.originalname.split(".");
+//       callback(
+//         null,
+//         file.originalname +
+//         Math.random() +
+//         "." +
+//         fileNameSplit[fileNameSplit.length - 1]
+//       );
+//     },
+//   });
+  
+//   var upload = multer({ storage: storage });
+  
 
 router.get("/bookings", async (req, res) => {
     const bookings = await Places.findAll();
@@ -98,9 +119,28 @@ router.put("/update/profileUserName/:id", async (req, res) => {
         res.json({ state: 0, error: "Complaint doesn't exist" });
     }
     res.json(updateProfile)
-    
-
 });
+
+// //update profile picture
+// router.put("/addImage/:id",upload.single("image"),
+// async (req, res) => {
+//     const image = req.file.filename;
+//     const userId = req.params.id;
+//     const details = req.body;
+//     console.log(userId)
+//     console.log(details)
+//     const updateProfile = await Occupants.update(
+//         {
+//             image : image,
+//         },
+//         {
+//             where: { id: userId }
+//         });
+//     if (!updateProfile) {
+//         res.json({ state: 0, error: "Complaint doesn't exist" });
+//     }
+//     res.json(updateProfile)
+// });
 
 
 module.exports = router;
